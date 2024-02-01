06aeb6e3bc86c0c048dc8cf470ee12c871cceb10
Aran <aranmcc7@outlook.com>
2024-02-01 18:11:33 +0000

    update nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,9 @@
 * @type {import('next').NextConfig}
 */
 const nextConfig = {
-    basePath: '/repository',
-    assetPrefix: '/repository/',
     output: 'export',
+    assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
     images: {
       loader: 'akamai',
       path: '',
